b5cc29a51410490eececce4fc83986262c584dc3
Jialuo Gao <jialuogao980513@gmail.com>
2018-12-09 16:21:11 -0600

    minor

--- kmeans/kmeans.m
+++ kmeans/kmeans.m
@@ -1,14 +1,16 @@
 clc
 close all
 clear
-img = imread('../img/7.jpg');
-style_img = imread('../output/generated_image.jpg');
+img = imread('../img/persian_cat.jpg');
+style_img = imread('../output/Persian Cat + Hokusai.jpg');
 img = double(img);
 img = imresize(img, 500/size(img,1));
 style_img = imresize(style_img, 500/size(style_img,1));
+% figure;imshow(uint8(img));imwrite(uint8(img), '../img/ppt/1.jpg')
+% figure;imshow(style_img);imwrite(uint8(style_img), '../img/ppt/2.jpg')
 
 densitymap = double(generateDensityMap(img, 40));
-% figure;imshow(detailmap/max(max(detailmap)));
+% figure;imshow(densitymap/max(max(densitymap)));imwrite(uint8(255*densitymap/max(max(densitymap))), '../img/ppt/3.jpg')
 
 [m, n, ~] = size(img);
 k = 2;
@@ -22,9 +24,11 @@ n = round(wy*n);
 densitymap = densitymap * 255 * wd / max(max(densitymap));
 d = round(max(max(densitymap)));
 [y, x] = find(img(:,:,1)<999999999);
-r = img(:,:,1);
-g = img(:,:,2);
-b = img(:,:,3);
+% figure;imshow(reshape(x/max(max(x)),size(img,1),size(img,2)));imwrite(uint8(255*reshape(x/max(max(x)),size(img,1),size(img,2))), '../img/ppt/4.jpg')
+% figure;imshow(reshape(y/max(max(y)),size(img,1),size(img,2)));imwrite(uint8(255*reshape(y/max(max(y)),size(img,1),size(img,2))), '../img/ppt/5.jpg')
+r = img(:,:,1);%imwrite(uint8(cat(3, img(:,:,1),zeros(size(img,1),size(img,2),2))), '../img/ppt/6.jpg')
+g = img(:,:,2);%imwrite(uint8(cat(3, zeros(size(img,1),size(img,2)), img(:,:,2),zeros(size(img,1),size(img,2)))), '../img/ppt/7.jpg')
+b = img(:,:,3);%imwrite(uint8(cat(3, zeros(size(img,1),size(img,2),2), img(:,:,3))), '../img/ppt/8.jpg')
 im = [r(:) g(:) b(:) wx*x wy*y wd*densitymap(:)];
 c1 = [randi([1 255]) randi([1 255]) randi([1 255]) randi([1 n]) randi([1 m]) randi([1 d])];
 c2 = [-1 -1 -1 -1 -1 -1];
@@ -53,8 +57,8 @@ im2 = ones(size(im(:,1:3)));
 im2(lable2,:) = 0;
 im1 = uint8(reshape(im1,size(img)));
 im2 = uint8(reshape(im2,size(img)));
-% figure; imshow(im1.*uint8(img));
-% figure; imshow(im2.*uint8(img));
+% figure; imshow(im1.*uint8(img));imwrite(uint8(im1.*uint8(img)), '../img/ppt/9.jpg')
+% figure; imshow(im2.*uint8(img));imwrite(uint8(im2.*uint8(img)), '../img/ppt/10.jpg')
 
 figure; imshow(uint8(img));
 while 1
